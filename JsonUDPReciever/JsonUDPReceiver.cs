bdb332aa90fb2de7706e5066abdfc21df320b267
Tobias S. Sørensen <Tss3331@hotmail.com>
2018-01-09 19:12:58 +0100

    JSONReciever

---
+++ JsonUDPReciever/JsonUDPReceiver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using ModelLib;
+using Newtonsoft.Json;
+
+namespace JsonUDPReciever
+{
+	class JsonUDPReceiver
+	{
+		private readonly int Port;
+
+		public JsonUDPReceiver(int port)
+		{
+			Port = port;
+		}
+
+		public void Start()
+		{
+			using (UdpClient client = new UdpClient(Port))
+			{
+				IPEndPoint AfsenderEp = new IPEndPoint(IPAddress.Any, 0);
+
+				byte[] receive = client.Receive(ref AfsenderEp);
+
+				string recivestr = Encoding.ASCII.GetString(receive);
+				var deserializedobject = JsonConvert.DeserializeObject<Car>(recivestr);
+
+
+				Console.WriteLine(deserializedobject);
+			}
+		}
+	}
+}
--- JsonUDPReciever/Program.cs
+++ JsonUDPReciever/Program.cs
@@ -8,8 +8,15 @@ namespace JsonUDPReciever
 {
 	class Program
 	{
+		private const int PORT = 11002;
 		static void Main(string[] args)
 		{
+
+			JsonUDPReceiver s = new JsonUDPReceiver(PORT);
+			s.Start();
+
+			Console.WriteLine("End of receiver program...");
+			Console.ReadLine();
 		}
 	}
 }
